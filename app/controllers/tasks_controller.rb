50119c1c65b307022185cbb910b97512ce6cd148
motoya <motoyao0118@gmail.com>
2022-07-17 09:21:25 +0900

    Fix Task_Controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %w[edit update show destroy]
   def index
     @tasks = Task.all
   end
@@ -9,31 +10,41 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
-    redirect_to tasks_path , notice: "登録しました"
+    if @task.save
+      redirect_to tasks_path , notice: "登録しました"
+    else
+      render :new
+    end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(task_params)
-    redirect_to tasks_path , notice: "編集しました"
+    if @task.update(task_params)
+      redirect_to tasks_path , notice: "編集しました"
+    else
+      render :edit
+    end
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    redirect_to tasks_path, notice: "削除しました"
+    if @task.destroy
+      redirect_to tasks_path, notice: "削除しました"
+    else
+      render :index
+    end
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:name,:content)
   end
